985fc24919e87070ed2d38f2fde4f51f666df28a
Luciano Fraga <lfraga@eshopworld.com>
2023-07-18 09:42:26 -0300

    Initial commit: Added: Home page; Firebase config;

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './home.styles.css'
+
+export const Home = () => {
+    return (
+        <div className="homeScreen">
+            <h2>Home Component</h2>
+            {/* Nav */}
+
+            {/* Banner */}
+
+            {/* Row */}
+        </div>
+    );
+}
